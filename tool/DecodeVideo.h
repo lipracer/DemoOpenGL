2f127fa4046e187d6b64dc7bd699c082db767824
chenlonglong <chenlonglong@qiyi.com>
2018-10-17 20:00:20 +0800

    add decodevideo

--- tool/DecodeVideo.h
+++ tool/DecodeVideo.h
@@ -1,1 +1,30 @@
 // create by lipracer
+
+
+
+
+class DecodeVide
+{
+public:
+    DecodeVideo();
+    DecodeVideo();
+    DecodeVideo(DecodeVideo& other);
+    DecodeVideo(DecodeVideo&& other);
+    DecodeVideo& operator=(DecodeVideo&);
+    DecodeVideo& operator=(DecodeVideo&&);
+private:
+public:
+private:
+    Bitmap m_bmp;
+    
+    
+    struct Bitmap
+    {
+        int m_width;
+        int m_height;
+        int depth;
+        int m_buf_len;
+        unsigned char *m_buf;
+    };
+
+}
--- tool/DecodeVideo.mm
+++ tool/DecodeVideo.mm
@@ -1,3 +1,42 @@
 // create by lipracer
 
 #include "DecodeVideo.h"
+
+
+DecodeVideo::DecodeVideo()
+{
+    
+}
+
+DecodeVideo::DecodeVideo()
+{
+    if(m_buf)
+    {
+        delete m_buf;
+    }
+}
+
+DecodeVideo::DecodeVideo(DevodeVideo& other)
+{
+    
+}
+
+DecodeVideo(DecodeVideo&& other)
+{
+    
+}
+
+DecodeVideo& DecodeVideo::operator=(DecodeVideo& other)
+{
+    if(this == &other) return *this;
+    
+    unsigned char *old_buf = m_buf;
+    m_buf = new unsigned char[];
+    delete old_buf;
+    return *this;
+}
+
+DecodeVideo& DecodeVideo::operator=(DecodeVideo&&)
+{
+    
+}
